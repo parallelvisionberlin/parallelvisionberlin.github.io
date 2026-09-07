07a9d6844fd9578077e2f25e77c2b518f7b14e0a
parallelvisionberlin <parallelvision.label@gmail.com>
2026-09-07 17:17:34 +0200

    Add mobile backend configuration

---
+++ mobile-app/src/config.js
@@ -0,0 +1,5 @@
+export const config = {
+  siteUrl: 'https://parallelvisionlabel.com',
+  ninaWorkerBaseUrl: 'https://parallel-vision-anam-token.parallelvision.workers.dev',
+  ninaWebUrl: 'https://parallelvisionlabel.com/nina-fok.html',
+};
