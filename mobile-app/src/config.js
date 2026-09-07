032cc0d87c9f1f8dfd03a6bff41825c1f959f241
parallelvisionberlin <parallelvision.label@gmail.com>
2026-09-07 17:18:55 +0200

    Point Nina CTA to live signal

--- mobile-app/src/config.js
+++ mobile-app/src/config.js
@@ -1,5 +1,6 @@
 export const config = {
   siteUrl: 'https://parallelvisionlabel.com',
   ninaWorkerBaseUrl: 'https://parallel-vision-anam-token.parallelvision.workers.dev',
-  ninaWebUrl: 'https://parallelvisionlabel.com/nina-fok.html',
+  ninaLiveUrl: 'https://parallelvisionlabel.com/nina.html',
+  ninaProjectUrl: 'https://parallelvisionlabel.com/nina-project.html',
 };
