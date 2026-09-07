8df29dc6573626e45497e668a558ac0ba6f632ab
parallelvisionberlin <parallelvision.label@gmail.com>
2026-09-08 01:59:00 +0200

    Add production Clerk public configuration

--- mobile-app/src/config.js
+++ mobile-app/src/config.js
@@ -3,4 +3,5 @@ export const config = {
   ninaWorkerBaseUrl: 'https://parallel-vision-anam-token.parallelvision.workers.dev',
   ninaLiveUrl: 'https://parallelvisionlabel.com/nina.html',
   ninaProjectUrl: 'https://parallelvisionlabel.com/nina-project.html',
+  clerkPublishableKey: 'pk_live_Y2xlcmsucGFyYWxsZWx2aXNpb25sYWJlbC5jb20k',
 };
